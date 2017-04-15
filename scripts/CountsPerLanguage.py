9e214dc2934a1c431764e65c001a5df28a82506a
Ravie Lakshmanan <ravielakshmanan@leh-101-6.barnard.columbia.edu>
2017-04-15 19:01:35 -0400

    Updated scripts

--- scripts/CountsPerLanguage.py
+++ scripts/CountsPerLanguage.py
@@ -2,9 +2,12 @@ import csv
 import json
 import pandas as pd
 
+file = "/Users/ravielakshmanan/Downloads/diversity/diversity_data.csv"
+datFrame = pd.read_csv(file, sep=';')
+dfTimeStamp = pd.to_datetime(datFrame['created_at'])
+datFrame['creationTime'] = dfTimeStamp;
+
 def getCounts(language):
-    file = "/Users/ravielakshmanan/Downloads/diversity/diversity_data.csv"
-    datFrame =  pd.read_csv(file, sep=';')
     df = datFrame[datFrame['language'] == language]
     df = df[["language","genders"]]
     d = {}
@@ -28,12 +31,33 @@ def getCounts(language):
                 d.update({language: (newFemaleCount, newMaleCount)})
             else:
                 d.update({language: (totalfCount[0], totalmCount[0])} )
+    return d
 
-    print(d)
+def getContributorsOverTime(startTime, endTime):
+    df = datFrame[(datFrame['creationTime'] >= startTime) & (datFrame['creationTime'] <= endTime )]
+    df = df[["genders"]]
+    d = {}
+    
+    totalfCount = []
+    totalmCount = [] 
+    for index, row in df.iterrows():
+        genders = row['genders']
+        genderArray = genders.split()
+        for gender in genderArray:
+            fCount = gender.count("female")
+            totalfCount.append(fCount)
+            mCount = gender.count("male")
+            totalmCount.append(mCount)
+            
+            d.update({"ContributorsOverTime": (sum(totalfCount), sum(totalmCount))})
+
     return d
 
 def main():
-    getCounts("Perl")
+    l = getCounts("Perl")
+    d = getContributorsOverTime("2012-11-23 17:26:24", "2012-11-23 17:26:26")
+    print(l)
+    print(d)
 
 if __name__== "__main__":
   main()
